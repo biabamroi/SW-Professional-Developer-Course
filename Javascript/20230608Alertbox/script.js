5971a9f3e8ac40f31a9ec0cea9999591deb6a02f
biabamroi <biabamroi@gmail.com>
2023-06-08 16:50:32 +0900

    🐛 오타

--- Javascript/20230608Alertbox/script.js
+++ Javascript/20230608Alertbox/script.js
@@ -6,4 +6,4 @@ function clossAlert(){
   document.getElementById('alert').style.display = 'none';
 }
 
-// document.getElementById('closs-btn').addEventListener('click', clossAlert);
+// document.getElementById('close-btn').addEventListener('click', clossAlert);
