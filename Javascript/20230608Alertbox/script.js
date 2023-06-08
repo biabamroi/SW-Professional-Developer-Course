a74a27f4f2e50be96e70c2db23e59747693a29b0
biabamroi <biabamroi@gmail.com>
2023-06-08 16:48:52 +0900

    ✨ alert창 open 기능 구현

---
+++ Javascript/20230608Alertbox/script.js
@@ -0,0 +1,9 @@
+document.getElementById('open-btn').addEventListener('click', function(){
+  document.getElementById('alert').style.display = 'flex';
+})
+
+function clossAlert(){
+  document.getElementById('alert').style.display = 'none';
+}
+
+// document.getElementById('closs-btn').addEventListener('click', clossAlert);
