5e040dd16b90af731cc764b863c4535336aa347b
Kai <biabamroi@gmail.com>
2023-06-11 04:32:07 +0900

    📝 drawer click open

--- 20230609Lego반응형/js/sidebarDrawer.js
+++ 20230609Lego반응형/js/sidebarDrawer.js
@@ -1,7 +1,27 @@
 const drawer = document.querySelector('.drawer');
-const drawer-opener = document.querySelector('.drawer-opener');
+const drawerOpener = document.querySelector('.drawer-opener');
+const drawer2 = document.querySelector('.drawer2');
+const drawerOpener2 = document.querySelector('.drawer-opener2');
+const drawer3 = document.querySelector('.drawer3');
+const drawerOpener3 = document.querySelector('.drawer-opener3');
 
-drawer-opener.addEventListener('click', function(){
-    drawer.classList.add('drawer-active');
+drawerOpener.addEventListener('click', function(){
+    drawer.style.display = 'block';
+})
+drawerOpener2.addEventListener('click', function(){
+    drawer2.style.display = 'block';
+})
+drawerOpener3.addEventListener('click', function(){
+    drawer3.style.display = 'block';
 })
 
+// drawerOpener.addEventListener('click', function(){
+//     drawer.classList.remove('.drawer-active');
+// })
+// document.getElementById('open-btn').addEventListener('click', function(){
+//     document.getElementById('alert').style.display = 'flex';
+//   })
+  
+//   function clossAlert(){
+//     document.getElementById('alert').style.display = 'none';
+//   }
