01a032d8b7f1391dba088af8739cc1d13f325703
Kai <biabamroi@gmail.com>
2023-06-11 04:45:19 +0900

    📝 임시 저장

--- 20230609Lego반응형/js/sidebarDrawer.js
+++ 20230609Lego반응형/js/sidebarDrawer.js
@@ -7,6 +7,8 @@ const drawerOpener3 = document.querySelector('.drawer-opener3');
 
 drawerOpener.addEventListener('click', function(){
     drawer.style.display = 'block';
+    .style.color = 'rgb(255, 204, 0)';
+
 })
 drawerOpener2.addEventListener('click', function(){
     drawer2.style.display = 'block';
@@ -14,6 +16,40 @@ drawerOpener2.addEventListener('click', function(){
 drawerOpener3.addEventListener('click', function(){
     drawer3.style.display = 'block';
 })
+
+
+
+
+
+
+var theParent = document.querySelector("#theDude");
+theParent.addEventListener("click", doSomething, false);
+ 
+function doSomething(e) {
+    if (e.target !== e.currentTarget) {
+        var clickedItem = e.target.id;
+        alert("Hello " + clickedItem);
+    }
+    e.stopPropagation();
+}
+
+
+
+var theParent = document.querySelector("#theDude");
+ 
+for (var i = 0; i < theParent.children.length; i++) {
+    var childElement = theParent.children[i];
+    childElement.addEventListener('click', doSomething, false);
+}
+ 
+function doSomething(e) {
+    var clickedItem = e.target.id;
+    alert("Hello " + clickedItem);
+}
+
+
+
+
 
 // drawerOpener.addEventListener('click', function(){
 //     drawer.classList.remove('.drawer-active');
