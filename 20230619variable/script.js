77dcb42cb6d18eacca5c1a363b759e2aed802e3a
Kai <biabamroi@gmail.com>
2023-06-19 13:14:50 +0900

    📝 복습 시작

---
+++ 20230619variable/script.js
@@ -0,0 +1,17 @@
+// var variable = "text";
+// var number = "0";
+// var boolean = true
+// scope 변수 어디 범위
+
+// function check () {
+//   console.log(boolean)
+// }
+
+var text = "text content"
+var app = document.getElementById('app')
+// console.log(app)
+// ctrl+shift+i 개발자 모드
+// app.innerHTML('text')
+// app.innerText('text')
+app.textContent = text;  // text만 넣는 경우 가장 빠른 
+
