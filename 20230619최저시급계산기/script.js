ff17292c7a4e0e0c180cd7552295460a8c3f13ab
Kai <biabamroi@gmail.com>
2023-06-19 16:22:47 +0900

    📝 정리

--- 20230619최저시급계산기/script.js
+++ 20230619최저시급계산기/script.js
@@ -23,7 +23,31 @@ actionElem.addEventListener('click', () => {
 
   // 결과 계산 = 근무시간 x 최저시급
   result = timeValue * salary;
-  resultElem.textContent = result
+  resultElem.textContent = result   // innerText, textContent 최적화
 })
 // console.log(result)
 
+
+
+// ES5
+// function func(number, text) {
+//   return console(number, text);
+// }
+
+// ES6+, ESNEXT(미래형)     --- ECMAScipt 단체 
+// const func = (number, text) => console.log(number, text)  // 축약
+
+// var 예기치 못한 상황 유발, 오류 발생 우려가 있어 점차 사용하지 않는 추세
+// let 변할 수 있는 값, 내용
+// const 변할 수 없는 값, 내용
+
+// console.log() 확인용
+// console.log(typeof variable(변수명))
+// number, string, boolean(참/거짓) 등 타입 체크
+// 타입 자료형 - 자료의 형태 - number, string, boolean, object...
+
+// addEventListener - 이벤트 청취, 관찰
+// 사용자가 특정 동작을 했을 때 
+// addEventListener('click', function(){})
+// mousemove, scroll, resize, mouseover, mouseleave, mouseenter...
+// touch event
