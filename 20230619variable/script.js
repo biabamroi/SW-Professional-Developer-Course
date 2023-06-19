97d1b44b1cd6ebe6909d4d7a6437fdd1bf8dc435
Kai <biabamroi@gmail.com>
2023-06-19 13:24:19 +0900

    📝scope

--- 20230619variable/script.js
+++ 20230619variable/script.js
@@ -7,11 +7,24 @@
 //   console.log(boolean)
 // }
 
-var text = "text content"
-var app = document.getElementById('app')
+// var text = "text content"
+const app = document.getElementById('app')
 // console.log(app)
 // ctrl+shift+i 개발자 모드
 // app.innerHTML('text')
 // app.innerText('text')
-app.textContent = text;  // text만 넣는 경우 가장 빠른 
+// app.textContent = text;  // text만 넣는 경우 가장 빠른 
+
+
+// var > ES5 버전
+// 현재 ES6  -  ESNEXT
+
+// const 변하지 않는 타겟
+// let 변하는 값 
+
+for(let i=0; i<10; i++){
+  console.log(i)
+} // scope 범위 지정 부분 확인해서 호출하여 사용
+const text = 'text';
+console.log(text)
 
