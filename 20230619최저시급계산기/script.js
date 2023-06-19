a61397c41846ce140a1eb54b3089be3c306c9198
Kai <biabamroi@gmail.com>
2023-06-19 15:44:01 +0900

    📝 scope 이동

--- 20230619최저시급계산기/script.js
+++ 20230619최저시급계산기/script.js
@@ -1,4 +1,27 @@
-const time = 0;
+
+// 최저시급 DOM
 const salaryElem = document.getElementById('salary')
-const salary = salaryElem.textContent
-console.log(salary)
+// 최저시급 content 값
+let salary = +salaryElem.textContent  // parseInt 정수로 바꿔줌, 축약어는 +
+// console.log(salary)
+// console.log(typeof salary) // 타입이 뭐냐고 - string 문자열
+
+// 결과 DOM
+const resultElem = document.getElementById('result')
+// 결과 content 값
+let result = resultElem.textContent
+// console.log(result)
+
+// 계산 버튼
+const actionElem = document.getElementById('action')
+actionElem.addEventListener('click', () => {
+  // 근무시간
+  const timeElem = document.getElementById('time')
+  let timeValue = +timeElem.value
+
+  // 결과 계산 = 근무시간 x 최저시급
+  result = timeValue * salary;
+  resultElem.textContent = result
+})
+// console.log(result)
+
