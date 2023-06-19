2c54180baec78b1f923ca035a5a2239378f9edc8
Kai <biabamroi@gmail.com>
2023-06-19 13:35:10 +0900

    📝 변수

--- 20230619variable/script.js
+++ 20230619variable/script.js
@@ -8,7 +8,7 @@
 // }
 
 // var text = "text content"
-const app = document.getElementById('app')
+// const app = document.getElementById('app')
 // console.log(app)
 // ctrl+shift+i 개발자 모드
 // app.innerHTML('text')
@@ -22,9 +22,27 @@ const app = document.getElementById('app')
 // const 변하지 않는 타겟
 // let 변하는 값 
 
-for(let i=0; i<10; i++){
-  console.log(i)
-} // scope 범위 지정 부분 확인해서 호출하여 사용
-const text = 'text';
-console.log(text)
+// for(let i=0; i<10; i++){
+//   console.log(i)
+// } // scope 범위 지정 부분 확인해서 호출하여 사용
+// const text = 'text';
+// console.log(text)
+
+
+
+// 버튼을 눌렀을 때, 값이 증가한다.
+// 1. 버튼을 변수에 저장하는 과정
+const btnInc = document.getElementById('increase')
+// console.log(btnInc)
+// 2. number 라는 변수에 id-num 저장
+const number = document.getElementById('num')   // const는 상수 : 변하지 않는 것
+// console.log(num)
+let incNum = 0    // 값 초기화
+
+// btnInc 눌렀을 때 숫자 증가
+btnInc.addEventListener('click', function(){
+  incNum = incNum + 1;   // 초기화된 값을 변화
+  // console.log(incNum)
+  number.textContent = incNum;
+})
 
