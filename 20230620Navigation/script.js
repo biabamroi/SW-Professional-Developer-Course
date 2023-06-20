ec7173764db30d10575a71488b3a3d6a84dc4dff
Kai <biabamroi@gmail.com>
2023-06-20 14:25:59 +0900

    📝 Array.from

--- 20230620Navigation/script.js
+++ 20230620Navigation/script.js
@@ -1,5 +1,12 @@
 const list = document.querySelector('.list')
-const listItem = list.querySelectorAll('.list-item')
+// Array.from('배열로 만들 내용') 
+const listItem = Array.from(list.querySelectorAll('.list-item'))
+
+// nodelist 배열인지 확인 필요
+// console.log(listItem)
+// listItem.forEach(item=>console.log(item))
+
+
 
 
 // for(let i = 0; i < 4; i++){
@@ -13,13 +20,15 @@ const listItem = list.querySelectorAll('.list-item')
 
 // const char = ['a','b','c','d']
 
-// i를 0,1,2,3으로 증가시키는 for문
+// // i를 0,1,2,3으로 증가시키는 for문
 // for(let i=0; i<char.length; i++){
 //   console.log('for문으로 돌린 결과'+char[i])
 // }
 
-// 배열 char 요소들 길이에 맞게 증가시킨 값을 가져옴
-// char.forEach((i)=>{
-//   console.log("forEach로 돌린 결과"+i)
+// // 배열 char 요소들 길이에 맞게 증가시킨 값을 가져옴
+// // forEach문에서는 item, index라는 매개 변수 2개를 제공한다.
+// char.forEach((item, index)=>{
+//   console.log("forEach로 돌린 결과"+item)
+//   console.log("forEach문의 index 값"+index)
 // })
 
