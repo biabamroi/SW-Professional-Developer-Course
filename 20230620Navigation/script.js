36ebc013449d8943e5b3ab9574271e65f277f430
Kai <biabamroi@gmail.com>
2023-06-20 14:19:37 +0900

    📝 for, forEach

--- 20230620Navigation/script.js
+++ 20230620Navigation/script.js
@@ -1,6 +1,7 @@
 const list = document.querySelector('.list')
 const listItem = list.querySelectorAll('.list-item')
 
+
 // for(let i = 0; i < 4; i++){
 //   console.log(i)
 // }
@@ -9,10 +10,16 @@ const listItem = list.querySelectorAll('.list-item')
 //   console.log(item, index)
 // })
 
-const char = ['a','b','c','d']
-for(let i=0; i<char.length; i++){
-  console.log('for문으로 돌린 결과'+char[i])
-}
-char.forEach((i)=>{
-  console.log("forEach로 돌린 결과"+i)
-})
+
+// const char = ['a','b','c','d']
+
+// i를 0,1,2,3으로 증가시키는 for문
+// for(let i=0; i<char.length; i++){
+//   console.log('for문으로 돌린 결과'+char[i])
+// }
+
+// 배열 char 요소들 길이에 맞게 증가시킨 값을 가져옴
+// char.forEach((i)=>{
+//   console.log("forEach로 돌린 결과"+i)
+// })
+
