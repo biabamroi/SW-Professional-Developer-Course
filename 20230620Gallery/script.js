ce35edabe63a68af82c8a176e2e8aa8665162bdd
Kai <biabamroi@gmail.com>
2023-06-20 16:10:58 +0900

    📝 Array, forEach

--- 20230620Gallery/script.js
+++ 20230620Gallery/script.js
@@ -3,11 +3,22 @@
 // ID 값을 찾을 때는 #ID 명
 const images = document.querySelector('.images')
 // const main = document.querySelector('main')
-const items = images.querySelectorAll('.image-item')
+
+// 배열 - Array.from(배열로 만들 리스트)
+const items = Array.from(images.querySelectorAll('.image-item'))
+// const items = images.querySelectorAll('.image-item')
 // console.log(items)
 
+// items 배열을 반복문으로 실행
+// forEach = for문의 축약형, 배열길이 감지, index값도 알아서 증가
+// 배열의 길이가 몇이건 알아서 길이에 맞게 반복
 items.forEach((item)=>{
+  // 배열 요소에서 사용자의 행동 감지
   item.addEventListener('click', ()=>{
-    console.log(item)
+    // console.log(item)
+    // 모든 아이템에서 active class 제거
+    items.forEach(item => item.classList.remove('active'))
+    // 해당 아이템에 active 부여
+    item.classList.add('active')
   })
 })
