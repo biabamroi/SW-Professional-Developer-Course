ddf55e83f7a147684b25a3a9b3f21fc39032342e
Kai <biabamroi@gmail.com>
2023-06-21 16:44:58 +0900

    📝 설명 추가

--- 20230621MouseMov/script.js
+++ 20230621MouseMov/script.js
@@ -9,8 +9,17 @@ window.addEventListener('mousemove',(event)=>{
   pointer.style.top = `${event.clientY - (pointer.offsetHeight/2)}px`;
   // 기존 방식 : pointer.style.top = event.clientX + 'px';
   pointer.style.left = `${event.clientX - (pointer.offsetWidth/2)}px`;
+  // ↑ event 객체에서 사용자의 x값을 가져와서 pointer의 가로값(36px)을 반으로 나눈 18px를 뺀다.
+  // ex) x = 100일 경우, 100-18, 82px, 커서는 위치가 82px, 결과적으로는 중앙으로 최종값
+
+
   // 커서 포인터와 오브젝트의 위치 파악 → 가운데 정렬 위한 수식 추가
+  
+  // clientWidth & Height 의 경우 margin, border 사이즈 무시
+  // border(1px solid white) 포함 36px 사이즈 인식 불가, 좌우상하 1px씩 제외한 34px만 인식
   // console.log(pointer.clientWidth)
+
+  // offserWidth는 border 사이즈도 포함해서 가져온다.
   // console.log(pointer.offsetWidth) 
 })
 
