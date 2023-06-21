cb6e0a8166b197ec7c9d8a912480183cceb6eff0
Kai <biabamroi@gmail.com>
2023-06-21 15:43:49 +0900

    📝 backtick 설명 추가

--- 20230621MouseMov/script.js
+++ 20230621MouseMov/script.js
@@ -7,6 +7,7 @@ window.addEventListener('mousemove',(event)=>{
   // ` 억음 부호 backtick
   // console.log(`마우스 X값 : ${event.clientX}, 마우스 Y값 : ${event.clientY}`)
   pointer.style.top = `${event.clientY}px`;
+  // 기존 방식 : pointer.style.top = event.clientX + 'px';
   pointer.style.left = `${event.clientX}px`;
 })
 
