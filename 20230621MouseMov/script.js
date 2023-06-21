86a764737edda42d6bc77af83c162e5d1e20ef07
Kai <biabamroi@gmail.com>
2023-06-21 15:37:18 +0900

    📝 console.log(backtick)

--- 20230621MouseMov/script.js
+++ 20230621MouseMov/script.js
@@ -2,5 +2,9 @@ const pointer = document.getElementById('pointer')
 // console.log(pointer)
 
 window.addEventListener('mousemove',(event)=>{
-  console.log(event.clientX, event.clientY)
+  // console.log(event.clientX, event.clientY)
+  // console.log('마우스 X값 : ' + event.clientX + ' 마우스 Y값 : ' + event.clientY)
+  // ` 억음 부호 backtick 
+  // console.log(`마우스 X값 : ${event.clientX}, 마우스 Y값 : ${event.clientY}`)
 })
+
