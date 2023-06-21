afe0e4c3e059fc7aa71be57bfc4e55a66c2eb7f0
Kai <biabamroi@gmail.com>
2023-06-21 15:41:29 +0900

    📝 pointer style event

--- 20230621MouseMov/script.js
+++ 20230621MouseMov/script.js
@@ -4,7 +4,9 @@ const pointer = document.getElementById('pointer')
 window.addEventListener('mousemove',(event)=>{
   // console.log(event.clientX, event.clientY)
   // console.log('마우스 X값 : ' + event.clientX + ' 마우스 Y값 : ' + event.clientY)
-  // ` 억음 부호 backtick 
+  // ` 억음 부호 backtick
   // console.log(`마우스 X값 : ${event.clientX}, 마우스 Y값 : ${event.clientY}`)
+  pointer.style.top = `${event.clientY}px`;
+  pointer.style.left = `${event.clientX}px`;
 })
 
