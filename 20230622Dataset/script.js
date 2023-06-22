5328f86a9d83cc0afdfd290a5072b7492641ea7e
Kai <biabamroi@gmail.com>
2023-06-22 15:25:11 +0900

    📝 javascript 연결

---
+++ 20230622Dataset/script.js
@@ -0,0 +1,5 @@
+const bar = document.querySelector('.bar')   // 오타!!! 항상 확인!!!
+// console.log('bar')
+// data-number, dataset.number
+const dataNumber = bar.dataset.number;
+console.log(dataNumber)
