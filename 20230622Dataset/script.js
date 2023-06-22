18d1e2526b5fde496a98da29a2eb4ac5ae8e1ffb
Kai <biabamroi@gmail.com>
2023-06-22 15:51:11 +0900

    📝 설명 추가

--- 20230622Dataset/script.js
+++ 20230622Dataset/script.js
@@ -1,12 +1,24 @@
 const container = document.querySelectorAll('.container')
 
 container.forEach(item => {
-  // console.log(item)
+  // console.log(item) // 확인 과정
+  // item : 부모, container
+  // container[0], container[1], container[2]
+  // 부모에서 bar를 찾고
   const bar = item.querySelector('.bar')   // 문서가 아니라 item에서 
+  // 부모에서 number를 찾고
   const number = item.querySelector('.number')
-  // console.log(bar.dataset.number)
+  // console.log(bar.dataset.number) // 확인 과정
+
+  // data-number 값을 접근할 때 dataset.number
+  // data-number값 가져와서 dataNumber 할당
   const dataNumber = bar.dataset.number;
+  // bar.data-numer : javascript 인지 불가
+  // bar.dataset.number : javascript 인지 가능
+
+  // number에 숫자 변경/삽입
   number.textContent = dataNumber;
+  // bar 그래프의 width 값으로 퍼센트 표현
   bar.style.width = dataNumber + '%'
 })
 
