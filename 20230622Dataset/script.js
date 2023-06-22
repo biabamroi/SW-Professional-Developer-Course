358fac106d67fba142ddc5efdef5fae1a11c5184
Kai <biabamroi@gmail.com>
2023-06-22 15:36:10 +0900

    📝 add graph

--- 20230622Dataset/script.js
+++ 20230622Dataset/script.js
@@ -1,10 +1,26 @@
-const bar = document.querySelector('.bar')   // 오타!!! 항상 확인!!!
+const container = document.querySelectorAll('.container')
+
+container.forEach(item => {
+  console.log(item)
+})
+
+
+
+
+
+// -------------------------------------------------------------------
+
+const bar = document.querySelector('.bar')     // 오타!!! 항상 확인!!!
 const number = document.querySelector('.number')
 // console.log('bar')
+
 // data-number, dataset.number
 const dataNumber = bar.dataset.number;
 // number에 숫자 변경/삽입
 number.textContent = dataNumber;
 // bar 그래프의 width 값으로 퍼센트 표현
 bar.style.width = dataNumber + '%'       // 자동완성 주의!!!
+
 console.log(dataNumber)
+
+
