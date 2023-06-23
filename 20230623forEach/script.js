9bce009b82b75063e55ed9851bc51b44aa4510bb
Kai <biabamroi@gmail.com>
2023-06-23 14:17:55 +0900

    📝 forEach index

--- 20230623forEach/script.js
+++ 20230623forEach/script.js
@@ -1,4 +1,19 @@
 const list = ['a','b','c','d','e']
-list.forEach((item)=>{
-  console.log(item)
+
+list.forEach((item, index) => {
+  console.log(item, index)
 })
+
+// forEach로 했을 때
+// list.forEach((item)=>{
+//   console.log(item)
+// })
+
+// lenght 확인용
+// console.log(list.length)
+
+// for로 했을 때
+// for(let index=0; index<list.length; index++){
+//   console.log(list[index])
+// }
+
