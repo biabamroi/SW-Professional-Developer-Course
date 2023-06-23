be513cea1c2398795982f33431f7aa075134b06d
Kai <biabamroi@gmail.com>
2023-06-23 13:28:30 +0900

    📝 view set

--- 20230623SetInterval/script.js
+++ 20230623SetInterval/script.js
@@ -1,7 +1,12 @@
+const number = document.querySelector('.number')
+
 let index = 0;
 // 1000 milliseconds - 1 second
 setInterval(()=>{
   index++
   // console.log(index)  // 콘솔 로그에서 수 증가 확인
+  number.textContent = index;
+  // console.log(index)  // 브라우저에서도 수 증가 확인
+  
 },1000)
 
