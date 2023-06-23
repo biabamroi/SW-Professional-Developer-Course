724692d67fed7f86118e5184978b2b6bae8bb3d8
Kai <biabamroi@gmail.com>
2023-06-23 14:37:15 +0900

    📝 complete

--- 20230623forEach/script.js
+++ 20230623forEach/script.js
@@ -1,33 +1,46 @@
 const app = document.getElementById('app')
 const list = ['a','b','c','d','e']
 
+
+// forEach로 했을 때 -----------------
 // 1. p 태그 생성
 // 2. p 태그 안에 item 문자열 삽입
 // 3. app 안에 p 삽입
 
-list.forEach((item, index) => {
-  const p = document.createElement('p')
-  p.textContent = item
-  app.appendChild(p)
-})
-console.log(app)
+// list.forEach((item, index) => {
+//   const p = document.createElement('p')
+//   p.textContent = item
+//   app.appendChild(p)
+// })
+// console.log(app)
 
 
-// index 출력
+// for로 했을 때 -----------------
+// for(let index=0; index<list.length; index++){
+//   const p = document.createElement('p')
+//   p.textContent = list[index]
+//   app.appendChild(p)
+// }
+// console.log(app)
+
+
+
+// index 출력 -----------------
 // list.forEach((item, index) => {
 //   console.log(item, index)
 // })
 
 
-// forEach로 했을 때
+
+// forEach로 했을 때 -----------------
 // list.forEach((item)=>{
 //   console.log(item)
 // })
 
-// lenght 확인용
+// lenght 확인용 -----------------
 // console.log(list.length)
 
-// for로 했을 때
+// for로 했을 때 -----------------
 // for(let index=0; index<list.length; index++){
 //   console.log(list[index])
 // }
