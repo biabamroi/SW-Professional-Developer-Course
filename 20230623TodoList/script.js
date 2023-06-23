0af45ac6d7babb60bc479babf46d184a4a465bae
Kai <biabamroi@gmail.com>
2023-06-23 16:19:04 +0900

    ♻️ styling update

--- 20230623TodoList/script.js
+++ 20230623TodoList/script.js
@@ -1,9 +1,14 @@
 const input = document.getElementById('add-todo')
 const addButton = document.getElementById('add-button')
-console.log(addButton)
+const list = document.getElementById('list')
 
 addButton.addEventListener('click',()=>{
-  console.log(input.value)
-  input.value = ''
+  // console.log(input.value)
+  const li = document.createElement('li')
+  li.innerHTML = `${input.value}
+  <button class="list-delete">&#x2716</button>`
+  li.classList.add('list-item')
+  list.appendChild(li)
+  input.value = '' // 위치 필히 확인, 마지막에 삭제해야 할 필요성
 })
 
