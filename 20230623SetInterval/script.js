d48ae897a4b0ac8ae46bf0cdb848c502204894c1
Kai <biabamroi@gmail.com>
2023-06-23 13:32:56 +0900

    📝 Btn start

--- 20230623SetInterval/script.js
+++ 20230623SetInterval/script.js
@@ -1,12 +1,25 @@
 const number = document.querySelector('.number')
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
 
 let index = 0;
-// 1000 milliseconds - 1 second
-setInterval(()=>{
-  index++
-  // console.log(index)  // 콘솔 로그에서 수 증가 확인
-  number.textContent = index;
-  // console.log(index)  // 브라우저에서도 수 증가 확인
-  
-},1000)
+let timerId;
+
+startBtn.addEventListener('click',() => {
+  timerId = setInterval(()=>{
+    index++
+    number.textContent = index;
+  },100)
+})
+
+
+// let index = 0;
+// // 1000 milliseconds - 1 second
+// setInterval(()=>{
+//   index++
+//   // console.log(index)  // 콘솔 로그에서 수 증가 확인
+//   number.textContent = index;
+//   // console.log(index)  // 브라우저에서도 수 증가 확인
+
+// },100)
 
