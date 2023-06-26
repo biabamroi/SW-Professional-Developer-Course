2079f53cb23a6188aaae183dc215879fa27ff36a
Kai <biabamroi@gmail.com>
2023-06-26 16:35:20 +0900

    📝 중간 저장

--- 20230626Accordion/script.js
+++ 20230626Accordion/script.js
@@ -6,9 +6,14 @@ const listItem = document.querySelectorAll('.list__item')
 // console.log(list, listItem)
 
 // listItem을 forEach 이용하여 item 요소들을 console.log로 보여주세요.
-
 listItem.forEach((item)=>{
-  console.log(item)
+  // listItem에서 요소들을 꺼내와서 consolo.log()
+  // console.log(item)
+
+  // 불러온 item을 클릭할 때마다 active 클래스 추가 또는 삭제
+  item.addEventListener('click',()=>{
+    item.classList.toggle('active')
+  })
 })
 
 
