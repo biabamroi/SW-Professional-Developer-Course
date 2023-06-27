64c8cc07240f7acc2cd3696f8367971f018e6021
Kai <biabamroi@gmail.com>
2023-06-27 13:39:20 +0900

    📝 toggle js

---
+++ 20230627SwitchToggle/script.js
@@ -0,0 +1,19 @@
+// 1. toggle-container 변수에 담기
+const toggle = document.querySelector('.toggle-container')
+console.log(toggle) // ('')는 문자열로 인식 
+
+let check = false;
+
+toggle.addEventListener('click',(e)=>{
+  // 자체적으로 갖고 있는 이벤트를 취소
+  e.preventDefault()
+  // 2. toggle-container 클릭 시 active 클래스 추가/삭제
+  toggle.classList.toggle('active')
+  if(check){
+    check = false
+    console.log(check)
+  }else{
+    check = true
+    console.log(check)
+  }
+})
