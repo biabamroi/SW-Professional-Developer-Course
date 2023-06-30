c74d226c377d55f2ac70cc7f3aa49130242ee4ac
Kai <biabamroi@gmail.com>
2023-06-30 16:12:54 +0900

    📝 components image

--- REACT/examreact/src/App.jsx
+++ REACT/examreact/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Flex from './components/Flex.jsx'
+import Image from './components/Image.jsx'
 
 const images = [
   {id: 1, url:'/cat1.jpg', title:'cat sleep'},
@@ -15,18 +16,16 @@ function App(){
     <>
     {/* map 배열 반환 */}
     {/* images && 이미지가 존재하면, (만일의 사태를 상정한 예외처리) */}
-    {/* 살펴봐야 하는 코드 if(!images) return (<p>이미지가 없습니다.</p>) */}
+    {/* 다시 살펴봐야 하는 코드 if(!images) return (<p>이미지가 없습니다.</p>) */}
     {/* key값 필수 부모가 있을 때는 부모한테 */}
     
     <Flex>
     {images && images.map(image =>
       (
-      <>
-      <figure className='image-wrapper' key={image.id}>
-        <img className='image' src={image.url} alt={image.title}/>
-      </figure>
+      <div key={image.id}>
+        <Image url={image.url} title={image.title}/>
       <h2>{image.title}</h2>
-      </>
+      </div>
       )
     )}
     </Flex>
