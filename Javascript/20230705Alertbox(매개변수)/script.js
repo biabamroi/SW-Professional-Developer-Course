e06c22a55f36c360c0ff5e6a689c9fe8e0428b80
Kai <biabamroi@gmail.com>
2023-07-05 15:06:31 +0900

    📝 onclick 매개변수

---
+++ Javascript/20230705Alertbox(매개변수)/script.js
@@ -0,0 +1,26 @@
+// function openAlert(){
+//   document.getElementById('alert').style.display = 'flex';
+// }
+
+// function clossAlert(){
+//   document.getElementById('alert').style.display = 'none';
+// }
+
+// 두 함수를 합칠 때
+
+function openNclose(switchs){
+  document.getElementById('alert').style.display = switchs;
+}
+
+
+
+
+// document.getElementById('open-btn').addEventListener('click', function(){
+//   document.getElementById('alert').style.display = 'flex';
+// })
+
+// function clossAlert(){
+//   document.getElementById('alert').style.display = 'none';
+// }
+
+// document.getElementById('close-btn').addEventListener('click', clossAlert);
