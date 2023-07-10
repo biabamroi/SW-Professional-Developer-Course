6ac7d9fe1dfdb0e4661fe5a37af89125bbef036a
Kai <biabamroi@gmail.com>
2023-07-10 13:35:48 +0900

    📝 조건문으로 button active class 추가/제거

--- 20230706slide/slide2/script.js
+++ 20230706slide/slide2/script.js
@@ -1,20 +1,41 @@
-let btnGroup = document.querySelectorAll('.btn-group button');
+let buttons = document.querySelectorAll('.btn-group button');
 let slideList = document.querySelector('.slide-list');
 
-btnGroup.forEach((btn,index)=>{
-  btn.addEventListener('click', ()=>{
-    btn.classList.toggle('active');
-    let xValue = -index * 100 + 'vw';
-    slideList.style.transform = `translateX(${xValue})`;
-  })  
-})
-
-let sBtnL = document.querySelector('.side-button-left');
-let sBtnR = document.querySelector('.side-button-right');
-
-btnGroup.forEach((btn,index)=>{
+buttons.forEach((btn,index)=>{
   btn.addEventListener('click', ()=>{
     let xValue = -index * 100 + 'vw';
     slideList.style.transform = `translateX(${xValue})`;
+    
+    // btnGroup 배열을 한 번 더 돌면서 모든 버튼이 갖고 있는
+    // active class remove
+    // buttons.forEach(function(otherBtn){
+    //   otherBtn.classList.remove('active');
+    // })
+    // 클릭된 버튼에만 active class add 
+    btn.classList.add('active');
+
+    // 조건문
+    // 현재 클릭된 버튼이 아닐 때 && active라는 class를 포함하고 있다면
+    // 위 조건이 true일 때 active class remove
+    buttons.forEach(function(otherBtn){
+      if(otherBtn !== btn && otherBtn.classList.contains('active')){
+        otherBtn.classList.remove('active');
+      }
+    })
+
   })  
 })
+
+
+
+
+
+// let sBtnL = document.querySelector('.side-button-left');
+// let sBtnR = document.querySelector('.side-button-right');
+
+// btnGroup.forEach((btn,index)=>{
+//   btn.addEventListener('click', ()=>{
+//     let xValue = -index * 100 + 'vw';
+//     slideList.style.transform = `translateX(${xValue})`;
+//   })  
+// })
