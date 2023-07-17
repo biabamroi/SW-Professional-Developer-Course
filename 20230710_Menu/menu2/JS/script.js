e5ec4181d087ed319ed5832d93c9206aa12f86fc
Kai <biabamroi@gmail.com>
2023-07-17 13:46:03 +0900

    ✨ menu2 on class add & remove

--- 20230710_Menu/menu2/JS/script.js
+++ 20230710_Menu/menu2/JS/script.js
@@ -15,6 +15,25 @@ $('.main-menu li, .submenu-wrapper').hover(function(){
   $('.submenu-wrapper').stop().slideUp(500);
 })
 
+// $('선택자').hover(function(){
+  // horver 시 실행
+// }, function(){
+  // leave 시 실행
+// })
+
+$('.submenu-list > li').hover(function(){
+  // .submenu-list > li 4개 중
+  // hover된 자신의 index 번호를 i라는 변수에 저장
+  let i = $(this).index();
+  // eq() : 인덱스 번호로 어떤 요소를 찾을 때 사용
+  $('.main-menu').find('a').eq(i).addClass('on');
+}, function(){
+  let i = $(this).index();
+  $('.main-menu').find('a').eq(i).removeClass('on');
+})
+
+
+
 
 
 
