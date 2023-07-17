6ee42051d679ff9d020b3c9e078fed4176362ed1
Kai <biabamroi@gmail.com>
2023-07-17 16:33:44 +0900

    ✨ popup self

---
+++ 20230711_HSFF실습/js/popup.js
@@ -0,0 +1,6 @@
+let popup = document.querySelector('.popup');
+let popupBtn = document.querySelector('.popup-btn');
+
+popupBtn.addEventListener('click', function(){
+  popup.style.display = 'none';
+})
