b7afd6941040babcd518440afbcd17da5ed38662
Kai <biabamroi@gmail.com>
2023-07-21 13:47:14 +0900

    ✨ dots 반복문으로 slide item 길이만큼 div 생성

--- 20230706slide/slide4(setInterval)/JS/script.js
+++ 20230706slide/slide4(setInterval)/JS/script.js
@@ -1,62 +1,9 @@
-let slideList = document.querySelector('.slide-list');
-let slideItems = document.querySelectorAll('.slide-list li');
-let slideItemContents = document.querySelectorAll('.slide-item-content');
+// 현재 인덱스를 저장하는 변수 (초기값 0)
+// 커렌트, 렝스 약자 사용
+let cur = 0;
+// .slide-item 클래스 요소 총 개수 저장
+let len = $('.slide-item').length;
 
-let slideBtn = document.querySelectorAll('.slide-btn');
-let slideBtn1 = document.getElementById('.slide-btn1');
-let slideBtn2 = document.getElementById('.slide-btn2');
-let slideBtn3 = document.getElementById('.slide-btn3');
-
-let prevBtn = document.querySelector('.left-btn');
-let nextBtn = document.querySelector('.right-btn');
-
-let pageChk = 0;
-let xValue = -index * 100 + 'vw';
-
-let SIContent = slideItemContents.forEach((){
-  
-})
-
-    // 만약 pageChk = 0 이라면 클래스 hidden을 remove하고
-    // 그 외의 index는 클래스 hidden을 add 한다.
-
-slideBtn.forEach((otherBtn,index)=>{
-  setInterval(function(){
-    if(pageChk === 0){
-      slideList.style.transform = `translateX(${xValue})`;
-      
-      return pageChk = 1;
-    }else if(pageChk === 1){
-      slideList.style.transform = 'translateX(-200vw)';
-
-      return pageChk = 2;
-    }else{
-      e.preventDefault();
-    }
-  }, 2000)
-})
-
-nextBtn.addEventListener('click', function(){
-  if(pageChk === 0){
-    slideList.style.transform = 'translateX(-100vw)';
-    return pageChk = 1;
-  }else if(pageChk === 1){
-    slideList.style.transform = 'translateX(-200vw)';
-    return pageChk = 2;
-  }else{
-    alert("마지막 페이지입니다.");
-    e.preventDefault();
-  }
-})
-
-prevBtn.addEventListener('click', function(){
-  if(pageChk === 0){
-    alert("첫 번째 페이지입니다.")
-  }else if(pageChk === 1){
-    slideList.style.transform = 'translateX(0vw)';
-    return pageChk = 0;
-  }else{
-    slideList.style.transform = 'translateX(-100vw)';
-    return pageChk = 1;
-  }
-})
+for(i=0; i<len; i++){
+  $('#dots').append('<div></div>');
+}
