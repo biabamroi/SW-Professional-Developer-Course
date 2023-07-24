6fbba869a32b269716b47392d93a32ebc30c0b33
Kai <biabamroi@gmail.com>
2023-07-24 14:48:16 +0900

    ✨ window scroll event

--- 20230724_ScrollEvent/JS/script.js
+++ 20230724_ScrollEvent/JS/script.js
@@ -20,4 +20,39 @@ window.addEventListener('scroll', function(){
   }
 })
 
-// 2. 
+let lorem = document.querySelector('.lorem');
+// 2. 스크롤이 끝까지 갔을 때 alert창으로 '스크롤 끝!'
+lorem.addEventListener('scroll', function(){
+  // scrollTop, clientHeight, scrollHeiht
+  // scrollTop : 스크롤된 양
+  let scrollAmount = document.querySelector('.lorem').scrollTop;
+  // clientHeight : 박스가 화면에 보이는 높이값
+  let displayHeight = document.querySelector('.lorem').clientHeight;
+  // scrollHeight : 박스에 스타일로 지정된 높이값이 아니라 스크롤 가능한 높이값을 나타낸다.
+  let scrollHeights = document.querySelector('.lorem').scrollHeight;
+  // console.log(scrollAmount, displayHeight, scrollHeights);
+
+  if(scrollAmount + displayHeight == scrollHeights){
+    alert('Scroll end!');
+  }
+})
+
+// window scroll 다 됐을 때 alert창 '전체 스크롤 완료!'
+window.addEventListener('scroll', function(){
+  // documentElement == querySelector('html') 동일
+  let 문서스크롤양 = document.querySelector('html').scrollTop;
+  let 문서높이 = document.querySelector('html').clientHeight;
+  let 문서스크롤높이 = document.querySelector('html').scrollHeight;
+  // console.log(문서스크롤양,문서높이,문서스크롤높이);
+  if(문서스크롤양+문서높이==문서스크롤높이){
+    alert('전체 문서 스크롤 완료!');
+  }
+})
+// 내가 짠 코드
+// window.addEventListener('scroll', function(){
+//   let all = document.querySelector('*');
+//   if(all.scrollTop + all.clientHeight == all.scrollHeight){
+//     alert('All Scroll Complete!');
+//   }
+//   console.log(all.scrollTop, all.clientHeight, all.scrollHeight);
+// })
