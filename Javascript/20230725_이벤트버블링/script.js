a330335f4abe13b223afac5dcdd7f4a2ed17deb7
Kai <biabamroi@gmail.com>
2023-07-25 16:48:07 +0900

    ✨ jQuery

--- Javascript/20230725_이벤트버블링/script.js
+++ Javascript/20230725_이벤트버블링/script.js
@@ -26,7 +26,11 @@ document.querySelector('.overlay').addEventListener('click', function(e){
 })
 
 
-
+$('.overlay').on('click',function(){
+  if($(e.target) == $('.overlay')){
+    $('.overlay').removeClass('is-open');
+  }
+})
 
 
 
