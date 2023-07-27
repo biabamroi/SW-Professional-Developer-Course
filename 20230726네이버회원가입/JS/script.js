e28512261115a69b63669fd28e1f9ed8ea13ade2
Kai <biabamroi@gmail.com>
2023-07-27 16:54:40 +0900

    🐛

--- 20230726네이버회원가입/JS/script.js
+++ 20230726네이버회원가입/JS/script.js
@@ -1,16 +1,16 @@
-// label 클릭했을 때
-// 클릭된 label의 자식인 .checkbox-img에 Checked class toggle
+// label 클릭 했을 때
+// 클릭된 label의 자식인 checkbox-img에 Checked class toggle
 $('label').on('click', function(e){
   e.preventDefault();
   $(this).children('.checkbox-img').toggleClass('checked');
 
-  // 클릭된 label의 자식인 .checkbox-img class에 
-  // checked라는 class가 포함되어 있다면
+  // 클릭된 label의 자식인 .checkbox-img class에
+  //  checked 라는 class가 포함되어 있다면 
   // <input type="checkbox">의 속성 checked
-  if($(this).children('.checkbox-img').hasClass('checked')){
-    // .checkbox-img.checkde 클래스가 있을 때
-    // 따옴표 중첩 사용 시 서로 다른 따옴료를 
-    $(this).children('input[type="checkbox"]').attr('checked', true);
-    $(this).children('input[type="checkbox"]').removeAttr('checked');
+  if($(this).children('.checkbox-img').hasClass('checked')) {
+    // .checkbox-img.checked 라는클래스가 있을 때
+    $(this).children("input[type='checkbox']").attr('checked', true);
+  } else {
+    $(this).children("input[type='checkbox']").removeAttr('checked');
   }
 })
