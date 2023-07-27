fbeb5a6314ee8ba77c343121a4f045818f855087
Kai <biabamroi@gmail.com>
2023-07-27 16:51:52 +0900

    ✨ label 클릭 이벤트 추가 (checked class & attr)

--- 20230726네이버회원가입/JS/script.js
+++ 20230726네이버회원가입/JS/script.js
@@ -1,31 +1,16 @@
-const total = document.querySelector('#checkerAll');
-const isChecked = total.checked;
-const checkbox = document.querySelectorAll('.checkbox-img');
+// label 클릭했을 때
+// 클릭된 label의 자식인 .checkbox-img에 Checked class toggle
+$('label').on('click', function(e){
+  e.preventDefault();
+  $(this).children('.checkbox-img').toggleClass('checked');
 
-total.addEventListener('click',function(){
-  if(isChecked){
-    const checkboxes = document.querySelectorAll('.checkbox-img');
-    for(const checkbox of checkboxes){
-    checkbox.checked = true;
-    }
-  }else{
-    const checkboxes = document.querySelectorAll('.checkbox-img');
-    for(const checkbox of checkboxes){
-      checkbox.checked = false;
-    }
+  // 클릭된 label의 자식인 .checkbox-img class에 
+  // checked라는 class가 포함되어 있다면
+  // <input type="checkbox">의 속성 checked
+  if($(this).children('.checkbox-img').hasClass('checked')){
+    // .checkbox-img.checkde 클래스가 있을 때
+    // 따옴표 중첩 사용 시 서로 다른 따옴료를 
+    $(this).children('input[type="checkbox"]').attr('checked', true);
+    $(this).children('input[type="checkbox"]').removeAttr('checked');
   }
 })
-
-const checkboxes = document.querySelectorAll('.checkbox-img');
-for(const checkbox of checkboxes){
-    checkbox.addEventListener('click',function(){
-        const totalCnt = checkboxes.length;
-        const checkedCnt = document.querySelectorAll('.checkbox-img:checked').length;
-        if(totalCnt == checkedCnt){
-            document.querySelector('#checkAll').checked = true;
-        }
-        else{
-            document.querySelector('#checkAll').checked = false;
-        }  
-    })
-}
