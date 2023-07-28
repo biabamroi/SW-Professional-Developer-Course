6173df44ba0e6bf7ad2f7f9c4f0f04934fd7421f
Kai <biabamroi@gmail.com>
2023-07-28 13:14:30 +0900

    ✨ total 체크 시 .agree .checkbox-img에 addClass

--- 20230726네이버회원가입/JS/script.js
+++ 20230726네이버회원가입/JS/script.js
@@ -14,3 +14,25 @@ $('label').on('click', function(e){
     $(this).children("input[type='checkbox']").removeAttr('checked');
   }
 })
+
+
+// .total을 체크하면 .agree 올 체크
+// .tatal을 언체크하면 .agree 전부 언체크
+
+// .total .checkbox-img가 .checked 클래스를 가지고 있다면,
+// .agree 안에 있는 .checkbox-img에 checked class add
+// .agree 안에 있는 input type checkbox를 checked true 
+
+// .total .checkbox-img가 .checked 클래스를 가지고 있지 않다면,
+// .agree 안에 있는 .checkbox-img에 checked class remove
+// .agree 안에 있는 input checkbox를 checked false
+
+$('.total label').on('click', function(){
+  // 조건식 $('.total label')의 자식인 ('.checkbox-img')에 ('.checked')가 있는가?
+  if($(this).children('.checkbox-img').hasClass('checked')){
+    // 참이면(있으면) $('.agree')에 ('.checkbox-img')를 찾아서 ('checked')를 추가해 줘라.
+    $('.agree').find('.checkbox-img').addClass('checked');
+  }else{
+
+  }
+})
