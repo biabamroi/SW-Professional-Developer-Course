03ba17089b7c30b4e08f505e96966e7ae20cb22c
Kai <biabamroi@gmail.com>
2023-08-07 13:50:34 +0900

    ✨ Vanilla JS로 비밀번호 조건문 구현

--- 20230726네이버회원가입/JS/join.js
+++ 20230726네이버회원가입/JS/join.js
@@ -40,6 +40,29 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
 
 
 // 비밀번호
+document.querySelector('.userpw input').addEventListener('focusout', function(){
+  let userPw = this.value;
+  let pwExp = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/;
+
+  let pwWarn = document.querySelector('.userpw .warn');
+  let pwText = document.querySelector('.userpw .inputbox p');
+  let pwImg = document.querySelector('.userpw .inputbox img');
+
+  if(userPw.length == 0){
+    pwWarn.innerHTML = essenInfo;
+    pwText.innerHTML = '';
+    pwImg.src = "./images/m_icon_pw_step_01.png";
+  }else if(!pwExp.test(userPw)){
+    pwWarn.innerHTML = '<span class="text-red">8~16자 영문 대소문자, 숫자, 특수문자를 사용해주세요.</span>';
+    pwText.innerHTML = '<span class="text-red">사용불가</span>';
+    pwImg.src = "./images/m_icon_pw_step_10.png";
+  }else{
+    pwveri = true;
+    pwWarn.innerHTML = '';
+    pwText.innerHTML = '<span class="text-green">안전</span>';
+    pwImg.src = "./images/m_icon_pw_step_04.png";
+  }
+})
 
 
 // 비밀번호 재확인
