af0b51b30b67748db51c1b7dd56eb5ef7057bcdf
Kai <biabamroi@gmail.com>
2023-08-07 14:23:57 +0900

    ✨ Vanilla JS로 비밀번호 재확인 조건문 구현

--- 20230726네이버회원가입/JS/join.js
+++ 20230726네이버회원가입/JS/join.js
@@ -38,21 +38,25 @@ document.querySelector('.userid input').addEventListener('focusout', function(){
   }
 })
 
+// 하단에서 필요하여 전역 변수로 뺌
+let userPw = document.querySelector('.userpw input');
 
 // 비밀번호
-document.querySelector('.userpw input').addEventListener('focusout', function(){
-  let userPw = this.value;
+// document.querySelector('.userpw input')
+userPw.addEventListener('focusout', function(){
+  // let userPw = this.value;
+  let userPwVal = userPw.value;
   let pwExp = /^[A-Za-z0-9`~!@#\$%\^&\*\(\)\{\}\[\]\-_=\+\\|;:'"<>,\./\?]{8,20}$/;
 
   let pwWarn = document.querySelector('.userpw .warn');
   let pwText = document.querySelector('.userpw .inputbox p');
   let pwImg = document.querySelector('.userpw .inputbox img');
 
-  if(userPw.length == 0){
+  if(userPwVal.length == 0){
     pwWarn.innerHTML = essenInfo;
     pwText.innerHTML = '';
     pwImg.src = "./images/m_icon_pw_step_01.png";
-  }else if(!pwExp.test(userPw)){
+  }else if(!pwExp.test(userPwVal)){
     pwWarn.innerHTML = '<span class="text-red">8~16자 영문 대소문자, 숫자, 특수문자를 사용해주세요.</span>';
     pwText.innerHTML = '<span class="text-red">사용불가</span>';
     pwImg.src = "./images/m_icon_pw_step_10.png";
@@ -66,6 +70,25 @@ document.querySelector('.userpw input').addEventListener('focusout', function(){
 
 
 // 비밀번호 재확인
+document.querySelector('.userpw-chk input').addEventListener('focusout', function(){
+  let userpwChk = this.value;
+  let pwChkWarn = document.querySelector('.userpw-chk .warn');
+  let pwChkImg = document.querySelector('.userpw-chk .inputbox img');
+
+  if(userpwChk.length == 0){
+    pwChkWarn.innerHTML = essenInfo;
+    pwChkImg.src = './images/m_icon_pw_step_02.png';
+  }else if(userpwChk == userPw.value){
+    pwchkveri = true;
+    pwChkWarn.innerHTML = '';
+    pwChkImg.src = './images/m_icon_pw_step_07.png';
+  }else{
+    // 입력은 했는데 .userpw value랑 일치하지 않을 때
+    pwchkveri = false;
+    pwChkWarn.innerHTML = '<span class="text-red">비밀번호가 일치하지 않습니다.</span>';
+    pwChkImg.src = './images/m_icon_pw_step_02.png';
+  }
+})
 
 
 // 이름
