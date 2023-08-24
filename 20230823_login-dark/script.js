82c4fd74eda2b02c2c42b3ec05224d39f832d5ab
Kai <biabamroi@gmail.com>
2023-08-24 16:14:27 +0900

    ✨ 삼항연산자로 Dark / Light mode toggle

--- 20230823_login-dark/script.js
+++ 20230823_login-dark/script.js
@@ -5,6 +5,7 @@
 // }
 // x, y => x + y;
 let btnLog = document.querySelector('.btn-login');
+let darkToggle = document.getElementById('darkmode-toggle');
 
 document.querySelectorAll('.animate-input').forEach((e) => {
   // e : .animate-input class가진 값을 하나씩 떼서 넘겨준 값
@@ -48,3 +49,20 @@ document.querySelectorAll('.animate-input').forEach((e) => {
     }
   } 
 })
+
+
+
+// darkmode toggle
+// darkToggle 클릭했을 때 
+// 텍스트 변경 : Darkmode => Lightmode
+// body에 .dark class add
+
+darkToggle.onclick = (e)=>{
+  e.preventDefault();
+  let body = document.querySelector('body');
+  body.classList.toggle('dark');
+
+  // 삼항 연산자
+  // 조건 ? 참 : 거짓
+  darkToggle.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode';
+}
