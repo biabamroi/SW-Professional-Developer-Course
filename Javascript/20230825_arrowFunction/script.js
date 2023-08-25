3ea03eea46fb980a399bf9429491144327892f0f
Kai <biabamroi@gmail.com>
2023-08-25 13:49:17 +0900

    ✨  함수선언식과 화살표 함수에서 this 차이

--- Javascript/20230825_arrowFunction/script.js
+++ Javascript/20230825_arrowFunction/script.js
@@ -1,15 +1,15 @@
+// Arrow Fucntion(화살표 함수)
 // Javascript ES6
 
 // function(){}
 // () => {}
 
 // 함수 : 항상 목적에 맞게 사용
-// 1. 하나의 코드로 묶을 때
-// 
+// 1. 여러 코드를 하나의 코드로 묶을 때
 
 // Arrow Function의 장점
 // 1. 파라미터가 하나일 때 소괄호 생략 가능
-// 2. 코드가 줄바꿈 없이 한 줄이면 중괄호, return
+// 2. 코드가 줄바꿈 없이 한 줄이면 중괄호, return 생략 가능
 
 // 함수 선언식
 // function plus(num) {
@@ -28,13 +28,12 @@
 // })
 
 // Arrow Function
-// 
+// 매개변수가 하나고, 코드가 한 줄일때 (),{} 생략 가능
 // [1,2,3,4].forEach(n=>console.log(n));
 
 
 
 let btn = document.getElementById('btn');
-
 
 // this == e.currentTarget
 // btn.addEventListener('click', function(e){
@@ -51,6 +50,9 @@ let btn = document.getElementById('btn');
 
 // arrow function은 함수 내의 this 값을 변경시키지 않는다.  
 // └ 함수 밖의 초기 this값을 사용 
+// this != e.currentTarget
+// arrow function에서 this값 => 함수 밖 this값 그대로 사용
+
 // btn.addEventListener('click', (e)=>{
 //   // this.innerHTML = '버튼';
 //   e.currentTarget.innerHTML = '버튼';
@@ -59,4 +61,3 @@ let btn = document.getElementById('btn');
 // })
 
 // 함수 선언식이랑 arrow function의 this값이 다르다.
-// 
