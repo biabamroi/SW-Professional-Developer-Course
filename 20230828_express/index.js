71e2bcdb7c57ff5a01d6fe7292d3dde140e5703d
Kai <biabamroi@gmail.com>
2023-08-30 16:49:03 +0900

    ✨ collection에 저장된 데이터 data.ejs에 바인딩

--- 20230828_express/index.js
+++ 20230828_express/index.js
@@ -210,11 +210,10 @@ app.post('/add', function(requests, response){
 // .ejs 사용 세팅
 app.set('view engine', 'ejs');
 
-app.get('/add', function(requests,response){
+app.get('/add', function(requests, response){
   // post라는 collection에 저장된 데이터를 꺼낸다.
   db.collection('post').find().toArray(function(error, result){
-    console.log(result)
-  })
-
-  response.render('data.ejs', {log : result})
-})
+    console.log(result);
+    response.render('data.ejs', {log : result});
+  });
+});
