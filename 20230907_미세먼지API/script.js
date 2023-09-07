4a7808c67d02ec1a96e180f266ae4ebe3b98f679
Kai <biabamroi@gmail.com>
2023-09-07 16:25:24 +0900

    ✨ dataTime 가장 최근 데이터 가져오기

--- 20230907_미세먼지API/script.js
+++ 20230907_미세먼지API/script.js
@@ -9,35 +9,45 @@ queryParams += '&' + encodeURIComponent('searchCondition') + '=' + encodeURIComp
 
 xhr.open('GET', url + queryParams);
 
-xhr.onreadystatechange = function () {
-  if (this.readyState == 4) { // 서버에 응답 완료 상태를 확인하는 코드
-    if(this.status === 200) { // HTTP 상태 코드 : 200번대 (성공)
-      // 서버로부터 받은 JSON 형식의 문자열 데이터를 Javascript 객체로 변환
-      // responseText : 객체가 서버로부터 응답받은 문자열이 담긴 변수
-      let responsData = JSON.parse(this.responseText);
-      // console.log(responsData.response.body.items);
-
-      // responsData에서 원하는 데이터만 추출해서 html 표기
-      if(responsData.response.body.items) {
-        let items = responsData.response.body.items;
-        console.log(items)
-        let dataDisplay = document.getElementById('data');
-        for(let i=0; i<items.length; i++) {
-          let item = items[i];
-
-          if(item.cityName == '서구') {
-            let dataItem = document.createElement('div');
-            dataItem.innerHTML = item.cityName + ' 미세먼지 : ' + item.pm10Value;
-            dataDisplay.appendChild(dataItem);
+function updateData(){
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) { // 서버에 응답 완료 상태를 확인하는 코드
+      if(this.status === 200) { // HTTP 상태 코드 : 200번대 (성공)
+        // 서버로부터 받은 JSON 형식의 문자열 데이터를 Javascript 객체로 변환
+        // responseText : 객체가 서버로부터 응답받은 문자열이 담긴 변수
+        let responsData = JSON.parse(this.responseText);
+        // console.log(responsData.response.body.items);
+  
+        // responsData에서 원하는 데이터만 추출해서 html 표기
+        if(responsData.response.body.items) {
+          let items = responsData.response.body.items;
+          console.log(items)
+          let dataDisplay = document.getElementById('data');
+          // 가장 최근 데이터를 저장할 변수
+          // 의도적으로 해당 변수에 아무 데이터가 할당되지 않았음을 나타내기 위해서 null로 초기값 설정
+          let latestData = null;
+          for(let i=0; i<items.length; i++) {
+            let item = items[i];
+  
+            if(item.cityName == '서구') {
+              // dataTime 가장 최근 데이터 가져오기
+              if(!latestData || item.dataTime > latestData.dataTime){
+                latestData = item.dataTime;
+                let dataItem = document.createElement('div');
+                dataItem.innerHTML = item.cityName + ' 미세먼지 : ' + item.pm10Value;
+                dataDisplay.appendChild(dataItem);
+              }
+            }
           }
+        } else {
+          console.log('데이터 구조를 다시 확인해 주세요.'); 
         }
       } else {
-        console.log('데이터 구조를 다시 확인해 주세요.'); 
+        console.log('HTTP 요청 실패' + this.status);
       }
-    } else {
-      console.log('HTTP 요청 실패' + this.status);
     }
-  }
-};
+  };
+  xhr.send('');
+}
 
-xhr.send('');
+updateData();
