a11cc5fa6b1d08b82e587505a77c67b552afed84
Kai <biabamroi@gmail.com>
2023-09-15 15:49:27 +0900

    🎉 npx create-react-app react-basic

---
+++ React/MEMO.js
@@ -0,0 +1,11 @@
+// 리액트 환경 구축
+// cd React
+// node --version
+// npm --version
+// npx create-react-app react-basic
+// cd react-basic
+// npm start
+// Local:            http://localhost:3000
+//  On Your Network:  http://192.168.123.107:3000
+
+// VScode setting : Include Languages - add item - javascript, javascriptreact
