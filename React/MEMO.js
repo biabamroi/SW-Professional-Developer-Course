328f929f7ded37a1216e6b2c187b999b93519dd8
Kai <biabamroi@gmail.com>
2023-09-18 15:49:45 +0900

    📝

--- React/MEMO.js
+++ React/MEMO.js
@@ -11,3 +11,8 @@
 // VScode setting : Include Languages - add item - javascript, javascriptreact
 
 // ctrl + alt + n 환경설정 (f5)
+
+
+// Code Runner 확장기능 
+// ctrl+alt+N (OUTPUT) 값 확인 
+// 자바스크립트 코드 콘솔로그 바로 확인하는 기능
