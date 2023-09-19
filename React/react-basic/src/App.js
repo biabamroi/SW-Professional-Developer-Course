2bf54667e313d835528e598af867d41528d52822
Kai <biabamroi@gmail.com>
2023-09-19 15:44:32 +0900

    ✨ useState로 상태 변경

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import User from './component/User';
+import State from './component/State';
 
 function App() {
   return (
     <div className="App">
       <User/>
       <br/>
+      <State/>
     </div>
   );
 }
