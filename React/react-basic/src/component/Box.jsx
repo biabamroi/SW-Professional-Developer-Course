b06c6f122bf8495766b1ef2ec0640eb1518ff232
Kai <biabamroi@gmail.com>
2023-09-19 14:47:32 +0900

    📝

--- React/react-basic/src/component/Box.jsx
+++ React/react-basic/src/component/Box.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Box(){
   return(
-    <div>
+    <>
       <h1>1. component</h1>
       <div>react component</div>
-    </div>
+    </>
   )
 }
 
---
+++ React/react-basic/src/component/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function User(){
+  let name = 'Kai';
+  return(
+    <div>
+      <h2>안녕하세요, {name}님!</h2>
+    </div>
+  )
+}
+
+export default User;
