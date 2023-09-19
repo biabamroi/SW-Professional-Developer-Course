6f0c789073337dc8c0146a52440142dac3066fe4
Kai <biabamroi@gmail.com>
2023-09-19 15:09:09 +0900

    🔥

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import User from './component/User';
-import Practice from './component/Practice';
 
 function App() {
   return (
     <div className="App">
       <User/>
       <br/>
-      <Practice/>
     </div>
   );
 }
