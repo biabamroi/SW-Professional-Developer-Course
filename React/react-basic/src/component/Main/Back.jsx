8088d4095520469a4c8b1d7426621ce22d82e05c
Kai <biabamroi@gmail.com>
2023-09-21 16:09:03 +0900

    ✨ Back component 구현 완료

--- React/react-basic/src/component/Main/Back.jsx
+++ React/react-basic/src/component/Main/Back.jsx
@@ -25,7 +25,11 @@ function Box(props){
             // showBtn이 true일 때만 Button을 랜더링
             // &&(and) 우항, 좌항 둘 다 true일 경우 랜더링
             // 좌항 false일 경우 바로 false 반환 (랜더링x)
-            showBtn && <button className='front-btn' type='button'>button</button>
+            
+            // 이벤트 핸들러에 함수가 아닌 표현식이나 값을 바로 전달 했을 경우
+            // 컴포넌트가 재랜더링 될 때마다 해당 값이 실행된다.
+            // onClick 됐을 때 실행되게 하려면 이벤트 핸들러에 값을 함수로 전달해야 한다.
+            showBtn && <button className='front-btn' type='button' onClick={()=>alert('Hello, World!')}>button</button>
           }
         </div>
       </section>
