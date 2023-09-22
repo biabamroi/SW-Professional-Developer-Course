26150b07ea3f0b61c72004d63b20f2d506760366
Kai <biabamroi@gmail.com>
2023-09-22 14:23:21 +0900

    ✨ CommentLisx.jsx

--- React/react-basic/src/App.js
+++ React/react-basic/src/App.js
@@ -1,22 +1,30 @@
 import './App.css';
+
 // import User from './component/User';
 // import State from './component/State';
 // import Map from './component/Map';
-import Nav from './component/Header/Nav';
-import Back from './component/Main/Back';
-import Cards from './component/Main/Cards';
-import Footer from './component/Footer/Footer';
+
+// import Nav from './component/Header/Nav';
+// import Back from './component/Main/Back';
+// import Cards from './component/Main/Cards';
+// import Footer from './component/Footer/Footer';
+
+import CommentList from './component/CommentList';
 
 function App() {
   return (
     <div className="App">
+
       {/* <User/> */}
       {/* <State/> */}
       {/* <Map/> */}
-      <Nav/>
-      <Back/>
-      <Cards/>
-      <Footer/>
+
+      {/* <Nav/> */}
+      {/* <Back/> */}
+      {/* <Cards/> */}
+      {/* <Footer/> */}
+
+      <CommentList/>
     </div>
   );
 }
