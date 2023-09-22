7000824a4399385170b1d46af5e8dc8622b02a05
Kai <biabamroi@gmail.com>
2023-09-22 14:02:01 +0900

    💄 Cards component styling

--- React/react-basic/src/component/Main/Cards.jsx
+++ React/react-basic/src/component/Main/Cards.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import './Cards.css';
 
 const Cards = () => {
-  let data = [
-    {tittle : 'Title1', content : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ullam enim aspernatur vero, deleniti neque iusto sequi! Ea, error odit.'},
-    {tittle : 'Title2', content : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ullam enim aspernatur vero, deleniti neque iusto sequi! Ea, error odit.'},
-    {tittle : 'Title3', content : 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus ullam enim aspernatur vero, deleniti neque iusto sequi! Ea, error odit.'}
+  let data = [ 
+    {title : 'Title1', content : 'Lorem ipsum dolor sit amet.'},
+    {title : 'Title2', content : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, odit!'},
+    {title : 'Title3', content : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia aliquid praesentium neque nemo quod quia.'},
   ]
   return (
-    <div>
-      
-    </div>
+    <section className='post-card-list'>
+      {
+        data.map((item, index) => {
+          return (
+            <Card title={item.title} content={item.content} key={index}/>
+          )
+        })
+      }
+    </section>
   );
 };
 
-function Card() {
+function Card(props) {
   return (
-    
+    <div className='post-card'>
+      <h2>{props.title}</h2>
+      <p className='post-card-txt'>{props.content}</p>
+    </div>
   )
 }
 
