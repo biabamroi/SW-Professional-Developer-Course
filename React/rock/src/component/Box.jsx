9ff1d3d024a62a323b8ab49aaf8adacac8d0cf40
Kai <biabamroi@gmail.com>
2023-09-25 14:15:56 +0900

    ✨ 가위바위보

--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Box.css';
 
-const Box = () => {
+const Box = (props) => {
   return (
     <div>
-      
+      <h1>{props.title}</h1>
+      <h2>Rock</h2>
+      <img src="https://cdn.pixabay.com/photo/2014/12/22/00/01/stone-576620_1280.png" alt="Rock" />
+      <h2>Win</h2>
     </div>
   );
 };
