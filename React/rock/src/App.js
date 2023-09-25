686835f1388acea923132d679bc96b0c3938e389
Kai <biabamroi@gmail.com>
2023-09-25 14:37:55 +0900

    🚧 유저, 컴퓨터, 선택 및 결과 state 등 작업 중

--- React/rock/src/App.js
+++ React/rock/src/App.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Box from './component/Box';
 
+let choice = {
+  rock : {
+    name : 'Rock',
+    img : 'https://cdn.pixabay.com/photo/2014/12/22/00/01/stone-576620_1280.png'
+  },
+  paper : {
+    name : 'Paper',
+    img : 'https://cdn.pixabay.com/photo/2013/07/13/12/34/typewriter-159878_640.png'
+  },
+  scissors : {
+    name : 'Scissors',
+    img : 'https://cdn.pixabay.com/photo/2019/06/21/22/35/vintage-4290490_640.png'
+  }
+}
+
 function App() {
+  let [userSelect, setUserSelect] = useState(null);
+  let [comSelect, SetComSelect] = useState(null);
+  let [result, setResult] = useState('');
+
   return (
     <div className="App">
       <div className='box-list'>
@@ -9,9 +29,9 @@ function App() {
         <Box title="Computer"/>
       </div>
       <div className="btn-list">
-        <button type='button'>가위</button>
-        <button type='button'>바위</button>
-        <button type='button'>보</button>
+        <button type='button'>✌️</button>
+        <button type='button'>👊</button>
+        <button type='button'>🖐️</button>
       </div>
     </div>
   );
--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './Box.css';
 
 const Box = (props) => {
   return (
-    <div>
+    <div className='box-item'>
       <h1>{props.title}</h1>
       <h2>Rock</h2>
       <img src="https://cdn.pixabay.com/photo/2014/12/22/00/01/stone-576620_1280.png" alt="Rock" />
