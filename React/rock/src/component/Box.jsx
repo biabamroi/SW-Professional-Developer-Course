e58a9afa65e646fe97f419dfe4fd1f8a3ce5f7a4
Kai <biabamroi@gmail.com>
2023-09-26 13:30:29 +0900

    🚧 user & computer 선택값 바인딩

--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -4,9 +4,13 @@ const Box = (props) => {
   return (
     <div className='box-item'>
       <h1>{props.title}</h1>
-      <h2></h2>
-      <img src="" alt="" />
-      <h2></h2>
+      {/* 
+        null error 해결 => 조건부 렌더링
+        null이 아닐 때만 값이 렌더링 되도록
+      */}
+      <h2>{props.select && props.select.name}</h2>
+      <img src={props.select && props.select.img} alt={props.select && props.select.alt} />
+      <h2>{props.result}</h2>
     </div>
   );
 };
