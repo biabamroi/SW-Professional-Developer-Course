a33dc98e3fa14bfb2f65b12d1040e1c2077f264a
Kai <biabamroi@gmail.com>
2023-09-26 13:47:58 +0900

    ✨ 리액트로 가위바위보 구현

--- React/rock/src/component/Box.jsx
+++ React/rock/src/component/Box.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 const Box = (props) => {
+  let result;
+
+  if(
+    props.title === 'Computer' &&
+    props.result !== 'tie' &&
+    props.result !== ''
+  ){
+    result = props.result === 'win' ? 'lose' : 'win';
+  }else{
+    result = props.result;
+  }
+
   return (
-    <div className='box-item'>
+    <div className={`box-item ${result}`}>
       <h1>{props.title}</h1>
       {/* 
         null error 해결 => 조건부 렌더링
@@ -10,7 +22,7 @@ const Box = (props) => {
       */}
       <h2>{props.select && props.select.name}</h2>
       <img src={props.select && props.select.img} alt={props.select && props.select.name} />
-      <h2>{props.result}</h2>
+      <h2>{result}</h2>
     </div>
   );
 };
